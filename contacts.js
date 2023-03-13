bd5dc269486e591ed4e41e1a43d2bc4377a4efec
dkusnieruk <dariusz.kusnieruk@wp.pl>
2023-03-13 06:54:47 +0100

    update task
    log delete prettier

--- contacts.js
+++ contacts.js
@@ -1,43 +1,43 @@
 const fs = require(`fs`).promises;
-const contactsPath = "./db/contacts.json"
+const contactsPath = "./db/contacts.json";
 
-    const listContacts = async() => {
-    const data = await fs.readFile(contactsPath)
-   
-    return JSON.parse(data)
-}    
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
 
-    const getContactsById = async (contactID) =>{
-        const data =await listContacts();
-        
-        data.filter(single =>{
-            if(single.id.includes(contactID)){
-              
-                return single
-            }
-            else 
-            {
-                return false}
-        })
-        }
-    
-    const removeContact = async (contactID) =>{
-        const data = await listContacts();        
-        const newList = data.filter(
-            contact => contact.id != contactID
-        )
-    }    
+  return JSON.parse(data);
+};
 
-    const addContact = async (name, email , phone) =>{
-        const data = await listContacts();
-        const newContact = {id:`${data.length+1}`, name:name, email:email, phone:phone}
-        data.push(newContact);
+const getContactsById = async (contactID) => {
+  const data = await listContacts();
+
+  data.filter((single) => {
+    if (single.id.includes(contactID)) {
+      return single;
+    } else {
+      return false;
     }
+  });
+};
 
+const removeContact = async (contactID) => {
+  const data = await listContacts();
+  const newList = data.filter((contact) => contact.id != contactID);
+};
 
-module.exports ={
-    listContacts,
-    getContactsById,
-    removeContact,
-    addContact,
-}
+const addContact = async (name, email, phone) => {
+  const data = await listContacts();
+  const newContact = {
+    id: `${data.length + 1}`,
+    name: name,
+    email: email,
+    phone: phone,
+  };
+  data.push(newContact);
+};
+
+module.exports = {
+  listContacts,
+  getContactsById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 const argv = require("yargs").argv;
 
-const {listContacts, getContactsById, removeContact, addContact}  = require("./contacts.js");
+const {
+  listContacts,
+  getContactsById,
+  removeContact,
+  addContact,
+} = require("./contacts.js");
 
-function invokeAction({action, id , name, email, phone}) {
-    switch(action) {
-        case "list":
-        listContacts()
-        break;
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      listContacts();
+      break;
 
-        case "get":
-        getContactsById(id)
-        break;
+    case "get":
+      getContactsById(id);
+      break;
 
-        case "add":
-        addContact(name, email , phone)
-        break;
+    case "add":
+      addContact(name, email, phone);
+      break;
 
-        case "remove":
-        removeContact(id)
-        break;
+    case "remove":
+      removeContact(id);
+      break;
 
-        default:
-            console.warn("\x1B[31m Unknown action type!");
-    }
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
 }
 
-invokeAction(argv)
+invokeAction(argv);
